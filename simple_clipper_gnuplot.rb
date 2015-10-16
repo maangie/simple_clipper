99b25ea6b691ded1eb0a556b6525e71614a32167
maangie <maangie906@gmail.com>
2015-10-16 20:09:04 +0900

    cosme

--- simple_clipper_gnuplot.rb
+++ simple_clipper_gnuplot.rb
@@ -1,4 +1,4 @@
-require "gnuplot"
+require 'gnuplot'
 require 'clipper'
 require 'pry'
 
@@ -14,27 +14,27 @@ def gnuplot_array(clipper_array)
 end
 
 Gnuplot.open do |gp|
-  Gnuplot::Plot.new( gp ) do |plot|
+  Gnuplot::Plot.new(gp) do |plot|
     plot.xrange '[-10:210]'
     plot.yrange '[-10:110]'
     a = [[0, 0], [0, 100], [100, 100], [100, 0]]
     b = [[-5, 50], [200, 50], [100, 5]]
 
     plot.data << Gnuplot::DataSet.new(gnuplot_array(a)) do |ds|
-      ds.with = "lines"
+      ds.with = 'lines'
     end
 
     plot.data << Gnuplot::DataSet.new(gnuplot_array(b)) do |ds|
-      ds.with = "lines"
+      ds.with = 'lines'
     end
 
     clipper = Clipper::Clipper.new
     clipper.add_subject_polygon(a)
-    clipper.add_clip_polygon(b)    
+    clipper.add_clip_polygon(b)
     c = clipper.union(:non_zero, :non_zero)[0]
-    
+
     plot.data << Gnuplot::DataSet.new(gnuplot_array(c)) do |ds|
-      ds.with = "lines"
+      ds.with = 'lines'
       ds.linewidth = 4
     end
   end
